8b6990f0fd29ccf013b3b9ccdf67a826d809ca29
Peio <peio@peio.org>
2017-05-08 22:14:01 +0300

    Added ToDo

--- RunAllSpiders.py
+++ RunAllSpiders.py
@@ -5,6 +5,16 @@ from datetime import date, timedelta
 from glob import glob
 from sys import exit, path
 
+'''
+This script should do the following: 
+
+1. Run all spiders
+2. Check the validity of the output
+3. Notify or commit the new filed in the repo
+
+'''
+
+
 'Get the dates in the YYYY-mm-dd format'
 Today = date.today().strftime("%Y-%m-%d")
 Yesterday = (date.today() - timedelta(1)).strftime("%Y-%m-%d")
@@ -12,7 +22,5 @@ Yesterday = (date.today() - timedelta(1)).strftime("%Y-%m-%d")
 
 if platform.system() == 'Linux':
 	
-	# system('ls')
 	system('scrapy runspider Dnevnik/DnevnikSpider.py -o Dnevnik/Reports/Dnevnik-'+str(Yesterday)+'.json -t json')
 	system('scrapy runspider mediapool/MediapoolSpider.py -o Mediapool/Reports/Dnevnik-'+str(Today)+'.json -t json')
-	# scrapy runspider MediapoolSpider.py -o Mediapool-30-Apr-2017.json -t json
