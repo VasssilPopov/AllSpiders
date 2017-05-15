4a0ddc8caf3d518d3513cdf5fa5c6b97d0748d8a
Peio <peio@peio.org>
2017-05-15 23:06:50 +0300

    Debugging the url encoding

--- PIK/Cleaning.py
+++ PIK/Cleaning.py
@@ -15,15 +15,36 @@ else:
 from ScrapingHelpers import *
 from datetime import date, timedelta
 
-'$ scrapy crawl Dnevnik -o Dnevnik.json -t jsonlines'
-'scrapy runspider DnevnikSpider.py -o Dnevnik-28-Apr-2017.json -t json'
-
-'Get the yesterday date'
+'Get the Today adn Yesterday dates'
 Today = date.today().strftime("%Y-%m-%d")
 yesterday = date.today() - timedelta(1)
 Yesterday = yesterday.strftime("%Y-%m-%d")
 
-# Dnevnik za Yesterday
-json_datafile = 'Report/PIK-'+Today+'.json'
+# File to check
+json_datafile = 'Reports/PIK-'+Today+'.json'
 
 check_empty(json_datafile)
+
+
+exit()
+# print list_urls(json_datafile)
+# print type(read_ids(json_datafile)[1])
+
+
+
+# url = "http://pik.bg/%D0%BC%D0%BB%D0%B0%D0%B4%D0%B8%D1%8F%D1%82-%D1%81%D0%BE%D1%86%D0%B8%D0%B0%D0%BB%D0%B8%D1%81%D1%82-%D0%BA%D1%80%D1%83%D0%BC-%D0%B7%D0%B0%D1%80%D0%BA%D0%BE%D0%B2-%D0%BA%D0%BE%D1%80%D0%BD%D0%B5%D0%BB%D0%B8%D1%8F-%D0%BD%D0%B8%D0%BD%D0%BE%D0%B2%D0%B0-%D0%BF%D1%80%D0%B5%D0%B4%D0%BB%D0%BE%D0%B6%D0%B8-%D0%B7%D0%B0%D0%BC%D0%B5%D1%81%D1%82%D0%BD%D0%B8%D1%86%D0%B8%D1%82%D0%B5-%D1%81%D0%B8-%D0%BD%D0%B8%D0%B5-%D1%81%D0%B0%D0%BC%D0%BE-%D0%B3%D0%B8-%D0%B3%D0%BB%D0%B0%D1%81%D1%83%D0%B2%D0%B0-news657838.html"
+url = u"http://pik.bg/рокадите-в-бсп-продължават-нинова-си-избра-&quotстари-муцуни&quot-за-заместници-обновена-news657704.html"
+url_encoded = "http://pik.bg/%D0%B1%D0%B0%D1%80%D0%BE%D0%BC%D0%B5%D1%82%D1%8A%D1%80-%D1%81%D0%BE%D1%86%D0%B8%D0%BE%D0%BB%D0%BE%D0%B3-%D1%81-%D1%82%D0%B5%D0%B6%D0%BA%D0%B0-%D0%BF%D1%80%D0%BE%D0%B3%D0%BD%D0%BE%D0%B7%D0%B0-%D0%B7%D0%B0-%D0%B1%D1%81%D0%BF-%D1%8E%D1%80%D0%B8%D0%B9-%D0%B0%D1%81%D0%BB%D0%B0%D0%BD%D0%BE%D0%B2-%D0%B7%D0%B0-%D1%82%D1%80%D1%83%D1%81%D0%BE%D0%B2%D0%B5%D1%82%D0%B5-%D0%BD%D0%B0-%D0%BF%D0%BE%D0%B7%D0%B8%D1%82%D0%B0%D0%BD%D0%BE-news657981.html"
+
+print type(url)
+# print url.encode('utf-8')
+
+from urllib2  import  quote
+print quote( url.encode('utf-8') ).replace("%3A", ':').replace("%26quot",'')
+print url_encoded
+# print url.split("http://pik.bg/")[1]
+if quote( url.encode('utf-8') ).replace("%3A", ':').replace("%26quot",'')  not in read_ids(json_datafile):
+	print 'Not'
+else:
+	print 'Found'
+
