79d1e5c7e551276e54db9ecb028cff357c41a7ec
Peio <peio@peio.org>
2017-05-17 08:58:38 +0300

    Experimenting with solutions around  checking for already retrieved urls

--- PIK/Cleaning.py
+++ PIK/Cleaning.py
@@ -36,6 +36,12 @@ exit()
 url = u"http://pik.bg/рокадите-в-бсп-продължават-нинова-си-избра-&quotстари-муцуни&quot-за-заместници-обновена-news657704.html"
 url_encoded = "http://pik.bg/%D0%B1%D0%B0%D1%80%D0%BE%D0%BC%D0%B5%D1%82%D1%8A%D1%80-%D1%81%D0%BE%D1%86%D0%B8%D0%BE%D0%BB%D0%BE%D0%B3-%D1%81-%D1%82%D0%B5%D0%B6%D0%BA%D0%B0-%D0%BF%D1%80%D0%BE%D0%B3%D0%BD%D0%BE%D0%B7%D0%B0-%D0%B7%D0%B0-%D0%B1%D1%81%D0%BF-%D1%8E%D1%80%D0%B8%D0%B9-%D0%B0%D1%81%D0%BB%D0%B0%D0%BD%D0%BE%D0%B2-%D0%B7%D0%B0-%D1%82%D1%80%D1%83%D1%81%D0%BE%D0%B2%D0%B5%D1%82%D0%B5-%D0%BD%D0%B0-%D0%BF%D0%BE%D0%B7%D0%B8%D1%82%D0%B0%D0%BD%D0%BE-news657981.html"
 
+links_seen = [url_encoded, url_encoded]
+
+links_seen = map(lambda url: url.split('news')[1] ,links_seen)
+print links_seen
+
+
 print type(url)
 # print url.encode('utf-8')
 
