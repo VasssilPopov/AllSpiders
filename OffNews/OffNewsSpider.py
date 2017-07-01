c7984de5d0955fee1a3e1ee4ab6d5d73e4abb3b3
Vassil <vassilsa@gmail.com>
2017-07-01 09:55:55 +0200

    OffNews spider curent state and reports 2017 june

--- OffNews/OffNewsSpider.py
+++ OffNews/OffNewsSpider.py
@@ -8,8 +8,7 @@ from scrapy.exceptions import CloseSpider
 from scrapy.spiders import Rule
 # from scrapy.linkextractors import Linkextractor
 
-# scrapy runspider OffNewsSpider.py -o Reports/OffNews-2017-05-17.json -t jsonlines
-# RunIt 2017-05-17
+# Python RunOffNewsSpider.py 
 
 'Get the yesterday date'
 yesterday = date.today() - timedelta(1)
@@ -38,53 +37,43 @@ class OffNewsSpider(scrapy.Spider):
     name = "offnews"
     allowed_domains = ['m.offnews.bg']
 
-	# https://m.offnews.bg/2017-05-27/
+	# https://m.offnews.bg/2017-06-28/
     start_urls = ['https://m.offnews.bg/'+Yesterday+'/']
     custom_settings = {
 		'FEED_EXPORT_ENCODING': 'utf-8'
 	}
-    # rules = (
-		# # Extract links matching 'category.php' (but not matching 'subsection.php')
-		# # and follow links from them (since no callback means follow=True by default).
-		# Rule(LinkExtractor(allow=('m.offnews.bg', ), deny=('mamaninja.bg', ))),
-		# )
-
-    def parse(self, response):
+ 
+    def __init__(self):
 		# "Empty output file"
 		fileName="OffNews/Reports/OffNews-%s.json"%(Yesterday)
 		f = open(fileName, 'w').close()
+		print '-'*10,'OffNews v(1.0)','-'*10
 
+
+    def parse(self, response):
 		urls = response.xpath('//div[@class="content"]/div[@class="cat_list_s"]/div[@class="cat_list_s_int"]/div[@class="cat_list_s_title"]/a/@href').extract()
+		print "url: %s selected: %d" %(response.url, len(urls))
 		for url in urls:
-			# if url[-11:] not in self.links_seen:
-				# url = response.urljoin(url)
-				# yield scrapy.Request(url=url, callback=self.parse_details)
 			url = response.urljoin(url)
 			yield scrapy.Request(url=url, callback=self.parse_details)
 
 		# follow pagination link
-		# next_page_url = response.css('li.next > a::attr(href)').extract_first()
 		next_page_url = response.xpath('//div[@class="pageBox bb3y"]/a[@class="next"]/@href').extract_first()
 		if next_page_url != '#':
 			next_page_url = response.urljoin(next_page_url)
 			yield scrapy.Request(url=next_page_url, callback=self.parse)
-		print '-- DONE --'
-    def parse_details(self, response):
-		print '3.parse_details: '
 
-		url=response.url
-		
-		title = response.xpath('//div[@class="news_box"]/h1[@class="news_box_title"]/text()').extract_first()
-		textStrong = response.xpath('//div[@class="newsdet"]/p/strong/text()').extract_first()
-			
-		texts = response.xpath('//div[@class="newsdet"]/p/text()').extract()
-		if textStrong:
-			text = textStrong + u''.join(texts)
-		else:
-			text = u''.join(texts)
-		yield {
-				'url': url,
-				'title': title,
-				'text': text,
-				'date': Yesterday
+    def parse_details(self, response):
+        url=response.url
+        if (url.startswith('http://mamaninja.bg/')):
+		    return
+        title = response.xpath('//div[@class="news_box"]/h1[@class="news_box_title"]/text()').extract_first()
+        textStrong = response.xpath('//div[@class="newsdet"]/p/strong/text()').extract_first()
+        texts = response.xpath('//div[@class="newsdet"]/p/strong/text() | //div[@class="newsdet"]/p/text()').extract()
+        text = u''.join(texts)
+        yield {
+			'url': url,
+			'title': title,
+			'text': text,
+			'date': Yesterday
 		}
--- OffNews/RunItForYesterday.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-scrapy runspider OffNewsSpider.py -o Reports/OffNews-%1.json -t jsonlines 2> Logs/outputOffNews.txt
-@echo on
