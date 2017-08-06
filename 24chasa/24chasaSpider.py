240436f25345a72287d57d55a82d4806c4bc8ba3
Vassil <vassilsa@gmail.com>
2017-08-06 03:21:13 +0200

    24chasa data 2017 July

--- 24chasa/24chasaSpider.py
+++ 24chasa/24chasaSpider.py
@@ -5,6 +5,8 @@ import json
 import json_lines
 from scrapy.exceptions import CloseSpider
 import os
+import sys
+
 # scrapy runspider 24chasaSpider.py -o Reports/24chasa-2017-05-17.json -t jsonlines
 # RunIt 2017-05-17
 
@@ -13,7 +15,7 @@ import os
 # Yesterday = yesterday.strftime("%Y-%m-%d")
 today = date.today()
 Today = today.strftime("%Y-%m-%d")
-strToday = today.strftime("%d.%m.%Y")
+strToday = today.strftime("%Y.%m.%d")
 
 def read_ids(file):
 
@@ -43,41 +45,65 @@ def translateDateBG_EN(strDate):
 	
 class Chasa24Spider(scrapy.Spider):
 	name = "24chasa"
-	allowed_domains = ['www.24chasa.bg']
+	# allowed_domains = ['https://www.24chasa.bg']
+	allowed_domains = ['24chasa.bg']
 	start_urls = [
-		"https://www.24chasa.bg/novini"
+		"https://www.24chasa.bg/novini/",
+		# "https://www.24chasa.bg/sport/",
+		# "https://www.24chasa.bg/region/",
+		# "https://www.24chasa.bg/zdrave/",
+		# "https://www.24chasa.bg/mnenia/",
+		# "https://www.24chasa.bg/ojivlenie/",
 	]
 	custom_settings = {
-		'FEED_EXPORT_ENCODING': 'utf-8'
+		'FEED_EXPORT_ENCODING': 'utf-8',
+		'CONCURRENT_REQUESTS_PER_DOMAIN':'1',
+		'DOWNLOAD_DELAY':'5',
+		'COOKIES_ENABLED':'False',
+		'DEPTH_LIMIT':'4'
 	}
 	
 	def __init__(self):
+		print '__init__'
 		self.json_datafile = '24chasa/Reports/24chasa-'+Today+'.json'
 		self.links_seen = read_ids(self.json_datafile)
 		'take only the end of the Mediapool url. The number after the news string:'
 		self.links_seen = set(map(lambda url: url.split('article/')[1] , self.links_seen))
 		print '-'*10,'24 chasa v(1.0)','-'*10
 		print 'seen: %d'% (len(self.links_seen))
+		
+		
 	def parse(self, response):
-
+		print 'parse'
 		urls = response.xpath('//div[@class="entry-short"]/a/@href | //div[@class="entry-short last-item"]/a/@href').extract()
 
-		print "url: %s selected: %d" %(response.url, len(urls))
+		print "depth: %d, url: %s selected: %d" %((response.meta['depth'] + 1), response.url, len(urls))
+		# print "url: %s selected: %d" %( response.url, len(urls))
 		for url in urls:
 			code=url.split('article/')[1]
+			
 			if code not in self.links_seen:
 				self.links_seen.add(code)
-				url = response.urljoin(url)
-				yield scrapy.Request(url=url, callback=self.parse_details)
+			url = response.urljoin(url)
+
+			print '>>>', response.meta['depth'] ,url
+
+			yield scrapy.Request(url=url, callback=self.parse_details)
 
 		# follow pagination link
 		next_page_url=response.xpath('//div[@id="page-left-content"]/div[@class="widget-left widget-entries "]/div[@class="widget-content"]/div[@class="pagination"]/a[last()]/@href').extract_first()
 		if next_page_url:
+			if (response.meta['depth'] == '4'):
+				raise CloseSpider('Depth reached 4')
+
 			next_page_url = response.urljoin(next_page_url)
 			yield scrapy.Request(url=next_page_url, callback=self.parse)
 
 	def parse_details(self, response):
 
+		print 'parse_details'
+
+	
 		url     = response.url
 		title   = response.xpath('//div[@id="page-left-content"]/div[@class="article"]/div[@class="head"]/h1/text()').extract_first().strip()
 
@@ -85,20 +111,19 @@ class Chasa24Spider(scrapy.Spider):
 		
 		pubDate=response.xpath('//div[@id="page-left-content"]/div[@class="article"]/div[@class="head"]/span[@class="article-date"]/span/text()').extract_first()
 		# 21.06.2017 10:59
-		
-		articleDate=pubDate.split()[0]
+		(day, month, year)=pubDate.split(' ')[0].split('.')
+		articleDate='%s.%s.%s'%(year, month, day)
+
+		# articleDate=pubDate.split()[0]
 		# print 'artDate: %s url= %s ' %(articleDate, url)
 		# Filter on todays date
-		# print articleDate, strToday, (articleDate == strToday)
+		print '>>>>', articleDate, strToday, (articleDate == strToday), url
 		if (articleDate == strToday):
-			# print 'saved'
 			yield {
 				'url': url,
 				'title': title,
 				'text': article,
-				'date': pubDate
-
+				'date': articleDate
 			}
 		else:
-			raise CloseSpider('Index date changed')
-
+			pass
