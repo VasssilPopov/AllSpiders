f11fa5d67c1e8314b84b251a96e4e1fd9ed820bf
Vassil <vassilsa@gmail.com>
2017-08-06 03:46:39 +0200

    Dnevnik

--- Dnevnik/DnevnikSpider.py
+++ Dnevnik/DnevnikSpider.py
@@ -2,28 +2,29 @@
 import scrapy
 import os
 from datetime import date, timedelta
+from sys import exit, path
+import platform
+
+if platform.system() == 'Linux':
+	path.append('/home/peio/dev/AllSpiders/_LIBRARY/')
+elif platform.system() == 'Windows':
+	path.append('C:\STUDY_SPIDERS\_AllSpiders\_LIBRARY')
+else: 
+	print 'Unknown platform' 
+	exit() 
+
+from Dates import *
 import os.path
 
 
 yesterday = date.today() - timedelta(1)
 Yesterday = yesterday.strftime("%Y-%m-%d")
-translateMonth={u'януари':'january',u'февруари':'february',u'март':'march',u'април':'april',u'май':'may',u'юни':'june',u'юли':'july',u'август':'august',u'септември':'september',u'октомври':'october',u'ноември':'november',u'декември':'december'}
-def bgMonthstoNumber(monthName):
-    monthName=monthName.lower()
-    months= {u'януари':'01',u'февруари':'02', u'март':'03',
-             u'април':'04',u'май':'05', u'юни':'06',
-             u'юли':'07',u'август':'08', u'септември':'09',
-             u'октомври':'10',u'ноември':'11', u'декември':'12'}
-
-    if (monthName in months):
-        return months[monthName]
-    else:
-        return'??'
 	
 class DnevnikSpider(scrapy.Spider):
     name = "Dnevnik"
     # start_urls = ["http://m.dnevnik.bg/allnews/"+yesterday.strftime("%Y/%m/%d")]
     start_urls = ["http://dnevnik.bg/allnews/yesterday/"]
+    # start_urls = ["http://dnevnik.bg/allnews/2017/07/29/"]
     count = 0
     custom_settings = {
 		'FEED_EXPORT_ENCODING': 'utf-8',
@@ -50,16 +51,16 @@ class DnevnikSpider(scrapy.Spider):
 
 		title = response.xpath('//div[@class="content"]/h1/text() | //aside/div[@class="module"]/div[@class="site-block"]/div[@class="the-picture-summary article"]/h2/text()').extract_first()
 		
-		article=u' '.join(response.xpath('//div[@class="article-content"]/p/text()| //div[@class="article-content"]/h2/text() | //div[@class="article-content"]/p/a/text()| //div[@class="article-content"]/div[@class="story"]/p/text() | //div[@class="article-content"]/div[@class="story"]/text()').extract())
+		article=u' '.join(response.xpath('//div[@class="article-content"]/p/text()| //div[@class="article-content"]/h2/text() | //div[@class="article-content"]/p/a/text()| //div[@class="article-content"]/div[@class="story"]/p/text() | //div[@class="article-content"]/div[@class="story"]/text() | //div[@class="module"]/div[@class="site-block"] /div[@class="the-picture-summary article"]/p/text()').extract())
 		# 'extract and prepare Article date'
 		pubDate = response.xpath('//div[@class="article-tools"]/time/text()').extract_first()
-		dateParts=pubDate.split()
-		#tDate=translateMonth[dateParts[2]]
-		#articleDate = ('%s-%s-%s' %(dateParts[1],tDate,'20'+dateParts[3])).lower()
-		articleDate='%s.%s.%s'%('20'+dateParts[3],bgMonthstoNumber(dateParts[2]),dateParts[1])	
+
+		articleDate = dcDnevnik(pubDate)
+		
 		yesterdaysDate=yesterday.strftime("%Y.%m.%d")
 		self.count = self.count + 1
 		# Filter on todays date
+		print ++(self.count), articleDate, yesterdaysDate
 		if ((articleDate == yesterdaysDate) and ( len(article)) > 0):
 			yield {
 				'url': url,
