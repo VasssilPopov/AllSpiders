7f65a8e4b92a0dff7cff5a875b6039ae008c9b01
Peio Popov <peio@peio.org>
2019-06-28 18:20:33 +0000

    Update with variable names

--- Utils/bgnews_load_elastic.sh
+++ Utils/bgnews_load_elastic.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# Config
+ELASTIC_INDEX="bgnews"
+ELASTIC_AUTH="elastic:kx4dHm4HDf5eyoKaBkdgZIBb"
+ELASTIC_HOST="https://3840179f313943c88f940d99436f39f0.us-east-1.aws.found.io:9243"
+ALL_SPIDERS_DIR="/home/peio/dev/AllSpiders/"
+
 cat BgNews.sites | while read line 
+
 do 
  echo $line 
  sleep 3	
@@ -7,16 +15,17 @@ do
 #  "AllSpiders/$line/Reports/*.json"
 # echo  "AllSpiders/$line/Reports/*.json"
 
-#elasticsearch_loader --index bgnews  --http-auth elastic:kx4dHm4HDf5eyoKaBkdgZIBb --es-host https://3840179f313943c88f940d99436f39f0.us-east-1.aws.found.io:9243 --type _doc json AllSpiders/$line/Reports/*.json  --lines
+elasticsearch_loader --index $ELASTIC_INDEX  --http-auth $ELASTIC_AUTH --es-host $ELASTIC_HOST --type _doc json $ALL_SPIDERS_DIR/$line/Reports/*.json  --lines
 
 # For the EP election month only:
 # AllSpiders/Dnevnik/Reports/Dnevnik-2019-05-
 
-
-echo "Loading for May 2018 Only!"
-sleep 1
-elasticsearch_loader --index bgnews-may2019  --http-auth elastic:kx4dHm4HDf5eyoKaBkdgZIBb --es-host https://3840179f313943c88f940d99436f39f0.us-east-1.aws.found.io:9243 --type _doc json AllSpiders/$line/Reports/$line-2019-05-*.json  --lines
+#echo "Loading for May 2018 Only!"
+#sleep 1
+#elasticsearch_loader --index bgnews-may2019  --http-auth elastic:kx4dHm4HDf5eyoKaBkdgZIBb --es-host https://3840179f313943c88f940d99436f39f0.us-east-1.aws.found.io:9243 --type _doc json AllSpiders/$line/Reports/$line-2019-05-*.json  --lines
 
 
 
 done
+
+exit 0
